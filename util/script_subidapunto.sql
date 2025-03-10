7bffbe1f1ffad0286f5f2ff89d166258c7b3c861
Luis-GonzalesC <155851833+Luis-GonzalesC@users.noreply.github.com>
2025-03-10 04:00:18 +0100

    Script actualizado

--- util/script_subidapunto.sql
+++ util/script_subidapunto.sql
@@ -1,5 +1,6 @@
 CREATE SCHEMA subidapunto_bd;
 USE subidapunto_bd;
+DROP SCHEMA subidapunto_bd;
 
 CREATE TABLE usuarios(
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -18,13 +19,6 @@ CREATE TABLE mangas(
     fecha_agregada DATE,
     imagen VARCHAR(255)
 );
-SELECT * FROM mangas;
-CREATE TABLE coleccion(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	id_usuario INT,
-	CONSTRAINT FK_COLECCION_USUARIO FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
 
 CREATE TABLE pertenece(
 	id_manga INT,
@@ -32,7 +26,7 @@ CREATE TABLE pertenece(
 	CONSTRAINT FK_PERTENECE_MANGA FOREIGN KEY (id_manga) REFERENCES mangas(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT FK_PERTENECE_COLECCION FOREIGN KEY (id_coleccion) REFERENCES coleccion(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-SELECT * FROM favoritos;
+
 CREATE TABLE favoritos(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	id_usuario INT,
@@ -40,5 +34,3 @@ CREATE TABLE favoritos(
     CONSTRAINT FK_FAVORITO_USUARIO FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	CONSTRAINT FK_FAVORITO_MANGA FOREIGN KEY (id_manga) REFERENCES mangas(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-SELECT favoritos.id_manga FROM favoritos JOIN mangas ON favoritos.id_manga = mangas.id WHERE titulo = "One Piece";
